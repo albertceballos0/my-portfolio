aaebc5a75bbdd297918556985b62a397d1e7f73e
albertceballos0 <albertceballos37@gmail.com>
2024-11-06 00:54:43 +0100

    Actualizar configuración de Firebase y FIreStore para usar variables de entorno públicas

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -3,20 +3,20 @@ import { getFirestore } from 'firebase-admin/firestore';
 
 // Carga el archivo JSON de la cuenta de servicio
 const serviceAccount = {
-  type: process.env.SA_TYPE,
-  project_id: process.env.SA_PROJECT_ID,
-  private_key_id: process.env.SA_PRIVATE_KEY_ID,
-  private_key: process.env.SA_PRIVATE_KEY,
-  client_email: process.env.SA_CLIENT_EMAIL,
-  client_id: process.env.SA_CLIENT_ID,
-  auth_uri: process.env.SA_AUTH_URI,
-  token_uri: process.env.SA_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.SA_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.SA_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.SA_UNIVERSE_DOMAIN
+  type: process.env.NEXT_PUBLIC_SA_TYPE,
+  project_id: process.env.NEXT_PUBLIC_SA_PROJECT_ID,
+  private_key_id: process.env.NEXT_PUBLIC_SA_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_SA_PRIVATE_KEY,
+  client_email: process.env.NEXT_PUBLIC_SA_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_SA_CLIENT_ID,
+  auth_uri: process.env.NEXT_PUBLIC_SA_AUTH_URI,
+  token_uri: process.env.NEXT_PUBLIC_SA_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_SA_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.NEXT_PUBLIC_SA_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.NEXT_PUBLIC_SA_UNIVERSE_DOMAIN
 }
 
-console.log(process.env.SA_UNIVERSE_DOMAIN);
+console.log(process.env.NEXT_PUBLIC_SA_UNIVERSE_DOMAIN);
 // Inicializa Firebase Admin con las credenciales de la cuenta de servicio
 if (!admin.apps.length) {
     admin.initializeApp({
