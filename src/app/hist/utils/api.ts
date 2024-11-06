4b530cd81b53c17d9120dc2e94072b10d02eecff
albertceballos0 <albertceballos0@gmail.com>
2024-11-06 13:49:43 +0100

    Actualizar fetchHistoryData: utilizar encabezados para construir la URL de la API y mejorar la flexibilidad del entorno

--- src/app/hist/utils/api.ts
+++ src/app/hist/utils/api.ts
@@ -1,11 +1,18 @@
 'use server'
 
+import { headers } from 'next/headers';
 
 export const fetchHistoryData = async () => {
-    const response = await fetch(`${process.env.API_URL}/api/history`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return await response.json().then(data => data.data);
-  };
+  const headersData = await headers();
+  const host = headersData.get('host');
+  const protocol = headersData.get('x-forwarded-proto') || 'http'; // Usa 'https' en producción si es necesario
+  const currentURL = `${protocol}://${host}`;
+
+  console.log('currentURL', currentURL);
+  const response = await fetch(`${currentURL}/api/history`);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
   
+  return await response.json().then(data => data.data);
+};
