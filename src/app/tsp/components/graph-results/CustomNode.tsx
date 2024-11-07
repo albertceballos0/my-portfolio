4ac30de9904302558949ec8d6a8c18a69d2febf1
albertceballos0 <albertceballos0@gmail.com>
2024-11-07 16:17:52 +0100

    Agregar componente CustomNode para representar nodos personalizados en el grafo del algoritmo TSP

---
+++ src/app/tsp/components/graph-results/CustomNode.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+type TreeNodeNew = {
+    name: string;
+    attributes: {
+        upperBound: number;
+        lowerBound: number;
+        length: number;
+        path : string;
+        prunned: boolean;
+        best: boolean;
+    };
+    children: TreeNodeNew[];
+}
+
+const CustomNode = ({ nodeDatum, onClick }: { nodeDatum: TreeNodeNew, onClick : () => void  }) => {
+
+    return (
+        <foreignObject width={200} height={80} x={-100} y={-40}
+        onClick={onClick}
+        >
+        <div
+            className={`p-2 rounded-md shadow-md text-center text-sm cursor-pointer transition-colors
+            ${nodeDatum.attributes?.best ? 'bg-green-100 border-2 border-green-500' : 
+                nodeDatum.attributes?.prunned ? 'bg-red-100 border-2 border-red-500' : 'bg-white border-2 border-primary'}
+            `}
+        >
+            <strong className="block mb-1">{nodeDatum.name}</strong>
+            {nodeDatum.attributes && (
+            <>
+                <div className="text-xs truncate" style={{ maxWidth: '180px' }}>
+                    UB: {nodeDatum.attributes.upperBound}, LB: {nodeDatum.attributes.lowerBound}
+                </div>
+                <div className="text-xs truncate" style={{ maxWidth: '180px' }}>
+                    PATH: {nodeDatum.attributes.path}
+                </div>
+            </>
+            )}
+        </div>
+        
+        </foreignObject>
+  )
+}
+  
+
+export default CustomNode
