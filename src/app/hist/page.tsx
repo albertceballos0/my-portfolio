b2d7f26de3903c14f5ef14e98e3a6c22f510a655
albertceballos0 <albertceballos0@gmail.com>
2024-11-08 20:15:39 +0100

    Refactor HistorialPage: convertir a componente cliente y simplificar la lógica de carga de datos

--- src/app/hist/page.tsx
+++ src/app/hist/page.tsx
@@ -1,38 +1,22 @@
-'use server'
+'use client'
 
+import {  useEffect, useState } from 'react'
+import { fetchHistoryData } from './utils/api'
+import Historial from './components/HIstotrial'
 
-// app/history/page.tsx
-import { Suspense } from 'react';
-import HistoryTable from './components/HistoryTable';
-import ErrorBoundary from './components/ErrorBoundary'; // Componente para manejar errores
-import { fetchHistoryData } from './utils/api';
-import { Loader2 } from 'lucide-react';
+export default function HistorialPage() {
+  const [historialData, setHistorialData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
-export default async function HistoryPage() {
-  let historyData = [];
-  let error: string | null = null;
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchHistoryData()
+      setHistorialData(data)
+      setIsLoading(false)
+    }
+    fetchData()
+  },[])
 
-  try {
-    historyData = await fetchHistoryData();
-  } catch (e) {
-    error = e instanceof Error ? e.message : 'An unknown error occurred';
-  }
-
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Request History</h1>
-      
-      {error ? (
-        <ErrorBoundary />
-      ) : (
-        <Suspense fallback={
-          <div className='h-screen flex items-center justify-center'>
-            <Loader2 />
-          </div>
-        }>
-          <HistoryTable initialData={historyData} />
-        </Suspense>
-      )}
-    </div>
-  );
+  return <Historial initialData={historialData} isLoading={isLoading} />
+  
 }
