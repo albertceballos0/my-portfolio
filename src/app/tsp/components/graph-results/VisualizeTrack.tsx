e983e4cd89c982df1aeddfb392e91c36242930ef
albertceballos0 <albertceballos0@gmail.com>
2024-11-08 17:03:22 +0100

    Ajustar el diseño de VisualizeTrack para mejorar la presentación y la estructura del componente

--- src/app/tsp/components/graph-results/VisualizeTrack.tsx
+++ src/app/tsp/components/graph-results/VisualizeTrack.tsx
@@ -48,43 +48,42 @@ const VisualizeTrack = () => {
 
 
   return (
-    <Card>
-        <div className="flex justify-center items-center overflow-hidden h-full">
-            { loading && <Loader2 className="h-8 w-8 animate-spin text-primary" />} 
-            {!loading &&
-            <ForceGraph2D
-            graphData={{ nodes: nodes || [], links: links || [] }}
-            nodeLabel="id"
-            linkLabel="value"
-            linkDirectionalArrowLength={8}
-            linkDirectionalArrowRelPos={1}
-            linkCurvature={0.20}
-            linkColor={() => 'orange'}
-            nodeColor={(node) => getNodeColor({ id: String(node.id) })}
-            nodeCanvasObject={(node, ctx, globalScale) => {
-            const label = nodes?.find((nodeItem) => nodeItem.id === node.id)?.label.join(', ') || '';
-            const fontSize = 12 / globalScale;
-            ctx.font = `${fontSize}px Sans-Serif`;
-            ctx.fillStyle = 'black';
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
-            if (node.x && node.y) {
-                ctx.fillText(label, node.x, node.y + 10);
-                ctx.fillText(String(node.id), node.x, node.y + 20);
-            }
+    <div className='flex flex-col justify-center items-center rounded-sm border border-gray-100 shadow-md'>
+      <div className="flex justify-center items-center overflow-hidden h-full w-full">
+        { loading && <Loader2 className="h-8 w-8 animate-spin text-primary" />} 
+        {!loading &&
+        <ForceGraph2D
+        graphData={{ nodes: nodes || [], links: links || [] }}
+        nodeLabel="id"
+        linkLabel="value"
+        linkDirectionalArrowLength={8}
+        linkDirectionalArrowRelPos={1}
+        linkCurvature={0.20}
+        linkColor={() => 'orange'}
+        nodeColor={(node) => getNodeColor({ id: String(node.id) })}
+        nodeCanvasObject={(node, ctx, globalScale) => {
+        const label = nodes?.find((nodeItem) => nodeItem.id === node.id)?.label.join(', ') || '';
+        const fontSize = 12 / globalScale;
+        ctx.font = `${fontSize}px Sans-Serif`;
+        ctx.fillStyle = 'black';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        if (node.x && node.y) {
+          ctx.fillText(label, node.x, node.y + 10);
+          ctx.fillText(String(node.id), node.x, node.y + 20);
+        }
 
-            // Draw a colored circle around the node
-            ctx.beginPath();
-            ctx.arc(node.x ?? 0, node.y ?? 0, 5, 0, 2 * Math.PI, false);
-            ctx.fillStyle = getNodeColor({ id: String(node.id) });
-            ctx.fill();
-            }}
-            enablePanInteraction={false} // Desactiva la capacidad de trasladar el grafo
-            height={400}
-            /> }
-        </div>
-    </Card>
-  
+        // Draw a colored circle around the node
+        ctx.beginPath();
+        ctx.arc(node.x ?? 0, node.y ?? 0, 5, 0, 2 * Math.PI, false);
+        ctx.fillStyle = getNodeColor({ id: String(node.id) });
+        ctx.fill();
+        }}
+        enablePanInteraction={false} // Desactiva la capacidad de trasladar el grafo
+        height={400}
+        /> }
+      </div>
+    </div>
   )
 }
 
