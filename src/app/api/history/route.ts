98a6bd37e1404d12f4216fb1a10accc127506e76
albertceballos0 <albertceballos0@gmail.com>
2024-11-11 13:59:06 +0100

    Agregar método POST en la API de historial para guardar solicitudes en la base de datos

--- src/app/api/history/route.ts
+++ src/app/api/history/route.ts
@@ -1,5 +1,5 @@
 import { db } from '@/lib/firestore';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { format } from 'date-fns';
 
 export async function GET() {
@@ -32,3 +32,32 @@ export async function GET() {
     }
 }
 
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+        console.log("Request body:", body);
+        const { request } = body;
+        // Save the request to the database
+        const docRef = db.collection('requests').doc();
+        try {
+            await docRef.set(request);
+        } catch (error) {
+            console.error("Error saving request to database:", error);
+            return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+            });
+        }
+        return new Response(JSON.stringify({ message: "Request saved successfully", id: docRef.id }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        console.error("Error parsing request body:", error);
+        return new Response(JSON.stringify({ message: "Invalid request body" }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+}
