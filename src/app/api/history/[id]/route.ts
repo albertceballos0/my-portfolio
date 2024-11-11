89ef57fce11063532050eb8dc7eef60a5e5523d7
albertceballos0 <albertceballos0@gmail.com>
2024-11-11 13:58:03 +0100

    Agregar función GET para recuperar entradas de historial y formatear marcas de tiempo

--- src/app/api/history/[id]/route.ts
+++ src/app/api/history/[id]/route.ts
@@ -1,6 +1,8 @@
 
 import { NextRequest, NextResponse } from 'next/server';
 import { db} from '@/lib/firestore';
+import { format } from 'date-fns';
+import { HistoryItem } from '@/types';
 
 
 export async function DELETE(req: NextRequest) {
@@ -17,4 +19,36 @@ export async function DELETE(req: NextRequest) {
         return NextResponse.json({ error: 'Failed to delete entry' }, { status: 500 });
     }
   }
-  
+
+
+export async function GET(req: NextRequest) {
+    const email = req.nextUrl.pathname.split('/').pop(); // Obtén el último segmento de la ruta
+    console.log('Getting entry:', email);
+    if (!email){
+        return NextResponse.json({ error: 'Email not found' }, { status: 404 });
+    }
+    try {
+        const snapshot = await db.collection('requests').where('user.email', '==', email).get();
+
+        if (snapshot.empty) {
+            return NextResponse.json({ data: [], success: true });
+        }
+        const historyEntries : HistoryItem[] = snapshot.docs.map(doc => {
+            const data = doc.data();
+            return {
+                id: doc.id,
+                user: data.user,
+                requestType: data.requestType,
+                timestamp: data.timestamp ? format(new Date(data.timestamp), 'yyyy-MM-dd HH:mm:ss') : ''
+            };
+        });
+        return NextResponse.json({ data: historyEntries, success: true });
+    } catch (error) {
+        console.error('Error al get la entrada:', error);
+        return NextResponse.json({ error: 'Failed to getting entry' }, { status: 500 });
+    }
+  }
+
+
+
+
