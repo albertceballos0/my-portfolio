183fe0d8db5ed89208f220cc8cae89d2edf2a7af
albertceballos0 <albertceballos0@gmail.com>
2024-11-11 13:58:08 +0100

    Refactor la página de historial para usar un hook personalizado y gestionar el estado global del historial y la autenticación del usuario

--- src/app/hist/page.tsx
+++ src/app/hist/page.tsx
@@ -1,22 +1,39 @@
 'use client'
 
-import {  useEffect, useState } from 'react'
-import { fetchHistoryData } from './utils/api'
+import { useEffect, useState } from 'react'
+import { useApi } from './hooks/useApi'
 import Historial from './components/HIstotrial'
+import { useHistStore } from '@/store/useHist'
+import { useAuthStore } from '@/store/useAuthStore'
 
 export default function HistorialPage() {
-  const [historialData, setHistorialData] = useState([])
+
+  //Estado de carga
   const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchHistoryData()
-      setHistorialData(data)
-      setIsLoading(false)
-    }
-    fetchData()
-  },[])
+  //Función para obtener el historial de la API con fetchHistoryData custom hook useApi
+  const { fetchHistoryData } = useApi();
 
-  return <Historial initialData={historialData} isLoading={isLoading} />
-  
+  //Estado global de historial y función para actualizarlo
+  const { hist, setHist, isInitialized } = useHistStore();
+  //Estado global de usuario
+  const { user } = useAuthStore();
+
+  //Obtener historial si no esta cargado y el usuario esta logueado
+  useEffect(() => {
+    //Función para obtener datos
+    const fetchData = async () => {
+      const data = await fetchHistoryData();
+      setHist(data);
+    };
+
+    if (!isInitialized) return; //Si no esta inicializado no se hace nada, sigue en estado de loading
+    //Si no hay historial (porque es la primera vez que clica el historial) y el usuario esta logueado se obtienen los datos
+    if (!hist && user) fetchData();
+    //Finalizar la carga
+    setIsLoading(false);
+  }, [hist, user]);
+
+  //Componente Historial con estado de carga
+  return <Historial isLoading={isLoading} />;
 }
